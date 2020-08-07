833798a2c6da8fad776499742455b6bc453c4204
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-08-07 12:07:09 +0200

    new 6

---
+++ 6 kyu/Break camelCase.js
@@ -0,0 +1,19 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// solution("camelCasing")  ==  "camel Casing" 
+
+
+
+
+function solution(string) {
+    let a = string.split('')
+    console.log(a)
+    for (let i = 0; i < a.length; i++){
+      if (a[i] === a[i].toUpperCase()) {
+        a.splice(i, 0, " ");
+        i += 2
+      }
+    }
+    return a.join('')
+}
