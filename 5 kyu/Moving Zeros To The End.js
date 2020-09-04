74cba1d69cffc785780cfb1f9f72741ddccfa81e
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-09-05 00:21:02 +0200

    5kyu

---
+++ 5 kyu/Moving Zeros To The End.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+  // TODO: Program me
+  const b = [];
+  const l = [];
+  for (let i in arr) {
+    arr[i] != 0 ? b.push(arr[i]) : l.push(arr[i]);
+  }
+  return b.concat(l);
+};
