597a79e7223efa69c51aab15e0d6d91807fc927d
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-09-04 14:01:47 +0200

    6kyu

---
+++ 6 kyu/Sum of Digits / Digital Root.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+  let num = n;
+  while (num > 9) {
+    let arr = num
+      .toString()
+      .split("")
+      .reduce((c, a) => {
+        return parseInt(c) + parseInt(a);
+      });
+    num = arr;
+  }
+  return num;
+}
