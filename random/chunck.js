dcb882c30e55c6cb29e812f6da0c7d6a1ead6d86
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-09-04 13:21:08 +0200

    chunked

---
+++ random/chunck.js
@@ -0,0 +1,38 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let e of array) {
+    const last = chunked[chunked.length - 1]; // las element
+    console.log(chunked[chunked.lenght - 1]);
+    if (!last || last.length === size) {
+      chunked.push([e]);
+    } else {
+      last.push(e);
+    }
+  }
+  return chunked;
+}
+
+// 2. solution
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    console.log(index);
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
