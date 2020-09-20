40d1e61b0e3b80d495ff245610574e2ca59906d6
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-09-20 20:00:03 +0200

    5kyu

---
+++ 5 kyu/Simple Pig Latin.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str) {
+  return str
+    .split(" ")
+    .map((i) => i.slice(1, i.length) + i.slice(0, 1) + "ay")
+    .join(" ");
+}
