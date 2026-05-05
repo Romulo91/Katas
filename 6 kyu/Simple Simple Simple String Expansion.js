59a03fd516d039215c930d7fc83c9209131cdf73
Galiao Goncalves <galiaogoncalves@kieback-peter.de>
2026-05-05 16:20:18 +0200

    6kata

---
+++ 6 kyu/Simple Simple Simple String Expansion.js
@@ -0,0 +1,46 @@
+// https://www.codewars.com/kata/5ae326342f8cbc72220000d2/train/javascript
+
+
+function stringExpansion(s) {
+    // Good luck!
+    if(s === "") return "";
+
+    let result = "";
+    let number = null
+
+    for(let char of  s) {
+        if(char >= "0" && char <= "9") {
+            number = Number(char)
+        } else {
+            const repeat = number === null ? 1 : number;
+            result += char.repeat(repeat)
+        }
+    }
+    return result
+}
+
+// TEST DATA
+
+stringExpansion('3abc') // 'aaabbbccc'
+stringExpansion('3D2a5d2f') // 'DDDaadddddf
+stringExpansion('0d0a0v0t0y') //
+stringExpansion('3d332f2a') // 'dddffa
+stringExpansion('abcde') // 'abcd
+stringExpansion('a2bcde') // 'abbccdde
+
+// Given a string that includes alphanumeric characters ("3a4B2d") return the expansion of that string: The numeric values represent the occurrence of each letter following that numeric value. There should be no numeric characters in the final string.
+//
+//     Notes
+// The first occurrence of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears
+// If there are multiple consecutive numeric characters, only the last one should be used (ignore the previous ones)
+// Empty strings should return an empty string.
+//     Your code should be able to work for both lower and capital case letters.
+//
+// "3D2a5d2f"  -->  "DDDaadddddff"    # basic example: 3 * "D" + 2 * "a" + 5 * "d" + 2 * "f"
+// "3abc"      -->  "aaabbbccc"       # not "aaabc", nor "abcabcabc"; 3 * "a" + 3 * "b" + 3 * "c"
+// "3d332f2a"  -->  "dddffaa"         # multiple consecutive digits: 3 * "d" + 2 * "f" + 2 * "a"
+// "abcde"     -->  "abcde"           # no digits
+// "1111"      -->  ""                # no characters to repeat
+// ""          -->  ""                # empty string
+
+
